7585b3a0fbfc9e766bf6bbe2273d0d21de35aa50
m-benrhabra <meriem.benrhabra1@gmail.com>
2020-07-21 20:34:28 +0100

    projet eco

---
+++ cozastore_Meriem/admin/add_admin.php
@@ -0,0 +1,140 @@
+
+<?php include "db.php" ?>
+<?php
+session_start();
+
+include "admin_header.php"
+ ?>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add admin data</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        
+        <form method="post" action="admin_register.php" >
+            <div class="modal-body">
+                <div class="form-group">
+                    <label >Username</label>
+                    <input type="text"  name="username" class="form-control" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label>Email address</label>
+                    <input type="email" name="email" class="form-control" aria-describedby="emailHelp" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" >
+                </div>
+                <div class="form-group">
+                    <label>Confirm Password</label>
+                    <input type="password" name="confpassword" class="form-control" >
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" name="register_btn" class="btn btn-primary">Save</button>
+            </div>  
+        </form>
+     
+      
+      </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="card shadow mb-4">
+        <div class="card-header by-3">
+            <h6 class="m-0 font-weight-bold text-primary"> Admin profile
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Add admin profile 
+                </button>
+            </h6>
+        </div>
+
+        <div class="card-body" >
+
+        <?php 
+        
+        if(isset($_SESSION['success']) && $_SESSION['success'] !=''){
+            echo "<div class='alert alert-success'>".$_SESSION['success']."</div>";
+            unset($_SESSION['success']);
+        }
+        if(isset($_SESSION['status']) && $_SESSION['status'] !=''){
+            echo "<div class='alert alert-danger'>".$_SESSION['status']."</div>";
+            unset($_SESSION['status']);
+        }
+
+        ?>
+            <div class="table-responsive">
+
+            <?php 
+            
+            $query = "SELECT * FROM register_admin";
+            $query_run = mysqli_query($db, $query);
+
+            ?>
+                <table class="table table-bordered table-hover" id="dataTable" width="100%" cillspacing="0">
+                    <thead>
+                        <tr>
+                                
+                            <th>Id</th>
+                            <th>Username</th>                       
+                            <th>Email</th>
+                            <th>Password</th>
+                            <th>EDIT</th>
+                            <th>DELETE</th>
+                                        
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    <?php 
+                    
+                    if(mysqli_num_rows($query_run) > 0){
+                        while($row = mysqli_fetch_assoc($query_run)){
+                    ?>
+                        <tr>
+                                
+                            <th><?php echo $row['id']; ?></th>
+                            <th><?php echo $row['username']; ?></th>                       
+                            <th><?php echo $row['email']; ?></th>
+                            <th><?php echo $row['password']; ?></th>
+                            <th>
+                            <form action="register_edit.php" method="post">
+                            <input type="hidden" name="edit_id" value="<?php echo $row['id']; ?>">
+                            <button type="submit" name="edit_btn" class="btn btn-success">EDIT</button>
+                            </form>
+                            </th>
+                            <th>
+                            <form action="admin_register.php" method="post">
+                            <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
+                            <button type="submit" name="delete_btn"  class="btn btn-danger">DELETE</button>
+                            </form>
+                            </th>
+                                        
+                        </tr>
+
+                    <?php
+                        }
+                    }else{
+                        echo "No record found";
+                    }
+                    
+                    ?>
+                    <tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include "admin_footer.php" ?>
---
+++ cozastore_Meriem/admin/add_product.php
@@ -0,0 +1,162 @@
+
+<?php include "admin_header.php"?>
+
+
+<?php 
+
+if (isset($_POST['add_product'])) {
+    $product_title = $_POST['product_title'];
+    $product_cat = $_POST['product_cat'];
+    $categories = $_POST['categories'];
+    $product_price = $_POST['product_price'];
+    $product_info = $_POST['product_info'];
+    $product_desc = $_POST['product_desc'];
+   
+    $product_img1 = $_FILES['product_img1']['name'];
+    $product_img2 = $_FILES['product_img2']['name'];
+    $product_img3 = $_FILES['product_img3']['name'];
+    $temp_name1 = $_FILES['product_img1']['tmp_name'];
+    $temp_name2 = $_FILES['product_img2']['tmp_name'];
+    $temp_name3 = $_FILES['product_img3']['tmp_name'];
+
+    move_uploaded_file($temp_name1, "../images/$product_img1");
+    move_uploaded_file($temp_name2, "../images/$product_img2");
+    move_uploaded_file($temp_name3, "../images/$product_img3");
+
+    $add_product = "INSERT INTO products (p_cat_id,cat_id,date,product_title,product_img1,product_img2,product_img3,product_price,product_info,product_desc) VALUES ('$product_cat','$categories',NOW(),'$product_title','$product_img1','$product_img2','$product_img3','$product_price','$product_info','$product_desc')";
+    $add_product_query = mysqli_query($db,$add_product);
+
+    if($add_product_query){
+        echo " <script>alert('product has been added successfully')</script>";
+        echo " <script>window.open('add_product.php','_self')</script>";
+    }
+
+}
+?>
+
+
+
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Add a product
+                            
+                           
+                        </h1>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+             <form action="add_product.php" method="post" enctype="multipart/form-data">    
+     
+     
+                    <div class="form-group">
+                        <label for="title">Product Title</label>
+                        <input type="text" class="form-control" name="product_title">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="title">Product categories </label>
+                        <!-- <input type="text" class="form-control" name="product_title"> -->
+                        <select name="product_cat" class="form-control" >
+                        
+                        <option> Select a category product </option>
+                          
+                          <?php 
+
+                           $get_p_cats = "SELECT * FROM product_categories";
+                           $run_p_cats = mysqli_query($db, $get_p_cats);
+                           while($row_p_cat= mysqli_fetch_array($run_p_cats)){
+                               $p_cat_id = $row_p_cat['p_cat_id'];
+                               $p_cat_title = $row_p_cat['p_cat_title'];
+
+                               echo "
+                               <option value=' $p_cat_id' > $p_cat_title </option>
+                               
+                               ";
+                           }
+                          ?>
+
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="title"> categories </label>
+                        <!-- <input type="text" class="form-control" name="product_title"> -->
+                        <select name="categories" class="form-control" >
+                        
+                        <option> Select a category </option>
+                          
+                          <?php 
+
+                           $get_cat = "SELECT * FROM categories";
+                           $run_cat = mysqli_query($db, $get_cat);
+                           while($row_cat= mysqli_fetch_array($run_cat)){
+                               $cat_id = $row_cat['cat_id'];
+                               $cat_title = $row_cat['cat_title'];
+
+                               echo "
+                               <option value=' $cat_id' > $cat_title </option>
+                               
+                               ";
+                           }
+                          ?>
+
+                        </select>
+                    </div>
+
+      
+                    <div class="form-group">
+                        <label for="product_image">Product Image 1</label>
+                        <input type="file"  name="product_img1" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="product_image">Product Image 2</label>
+                        <input type="file"  name="product_img2" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="product_image">Product Image 3</label>
+                        <input type="file"  name="product_img3" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="product_price">Product Price</label>
+                        <input type="number" class="form-control" name="product_price">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="product_info">Product Infos</label>
+                        <textarea class="form-control "name="product_info" id="" cols="30" rows="5">
+                        </textarea>
+                    </div>
+
+                    
+                    <div class="form-group">
+                        <label for="product_desc">Product Description</label>
+                        <textarea class="form-control "name="product_desc" id="" cols="30" rows="5">
+                        </textarea>
+                    </div>
+                    
+
+                    <div class="form-group">
+                        <input class="btn btn-primary" type="submit" name="add_product" value="Add product">
+                    </div>
+
+
+            </form>
+
+
+            </div>
+            <!-- /.container-fluid -->
+
+    <script src="js/tinymce/tinymce.min.js" ></script>
+    <script>tinymce.init({selector: 'textarea'});</script> 
+
+    <?php include "admin_footer.php" ?>
+   
